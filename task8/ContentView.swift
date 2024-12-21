2ac4797f3bc2c496bd88297e196d3e5587b23dae
mi11ione <i@mi11ion.io>
2024-12-21 03:08:39 +0300

    Initial Commit

---
+++ task8/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  task8
+//
+//  Created by mi11ion on 21/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ task8/task8App.swift
@@ -0,0 +1,17 @@
+//
+//  task8App.swift
+//  task8
+//
+//  Created by mi11ion on 21/12/24.
+//
+
+import SwiftUI
+
+@main
+struct task8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
